a0cfccd9445cbf3ce2e8bc8c0f0ce4555673d316
Saif Shaikh <shekhsaif480@gmail.com>
2024-03-31 02:11:13 +0530

    all

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,39 +1,39 @@
 import React from 'react'
-
+import "./Navbar.css"
 const Navbar = () => {
     return (
         <div>
 
             <nav
-                class="relative flex w-full flex-wrap items-center justify-between bg-zinc-50 py-2 shadow-dark-mild dark:bg-neutral-700 lg:py-4">
+                class="relative flex w-full flex-wrap items-center justify-between py-2 shadow-dark-mild" >
                 <div class="flex w-full flex-wrap items-center justify-between px-3">
                     <nav class="w-full rounded-md" aria-label="breadcrumb">
                         <ol class="list-reset ms-2 flex">
                             <li>
                                 <a
-                                    href="#"
-                                    class="text-black/60 transition duration-200 hover:text-black/80 hover:ease-in-out focus:text-black/80 active:text-black/80 motion-reduce:transition-none dark:text-white/60 dark:hover:text-white/80 dark:focus:text-white/80 dark:active:text-white/80"
+                                    href="Home"
+                                    class="text-3xl text-black/2000 transition duration-200 hover:text-black/80 hover:ease-in-out focus:text-black/80 active:text-black/80 motion-reduce:transition-none"
                                 >Home</a
                                 >
                             </li>
                             <li>
-                                <span class="mx-2 text-black/60 dark:text-white/60">/</span>
+                                <span class="mx-2 text-3xl text-black/60 ">/</span>
                             </li>
                             <li>
                                 <a
                                     href="#"
-                                    class="text-black/60 transition duration-200 hover:text-black/80 hover:ease-in-out focus:text-black/80 active:text-black/80 motion-reduce:transition-none dark:text-white/60 dark:hover:text-white/80 dark:focus:text-white/80 dark:active:text-white/80"
-                                >Library</a
+                                    class="text-3xl text-black/60 transition duration-200 hover:text-black/80 hover:ease-in-out focus:text-black/80 active:text-black/80 motion-reduce:transition-none"
+                                >About</a
                                 >
                             </li>
                             <li>
-                                <span class="mx-2 text-black/60 dark:text-white/60">/</span>
+                                <span class="mx-2 text-3xl text-black/60 ">/</span>
                             </li>
                             <li>
                                 <a
-                                    href="#"
-                                    class="text-black/60 transition duration-200 hover:text-black/80 hover:ease-in-out focus:text-black/80 active:text-black/80 motion-reduce:transition-none dark:text-white/60 dark:hover:text-white/80 dark:focus:text-white/80 dark:active:text-white/80"
-                                >Data</a
+                                    href="mailto:shaiktausif7557@gmail.com"
+                                    class="text-3xl text-black/60 transition duration-200 hover:text-black/80 hover:ease-in-out focus:text-black/80 active:text-black/80 motion-reduce:transition-none"
+                                >Contact Me</a
                                 >
                             </li>
                         </ol>
